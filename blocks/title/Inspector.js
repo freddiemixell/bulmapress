e1f233e36abc630347ac2d862c088c821f04b8f5
freddiemixell <fmixell@gmail.com>
2020-01-19 09:24:45 -0500

    font and color controls

--- blocks/title/Inspector.js
+++ blocks/title/Inspector.js
@@ -5,6 +5,7 @@ const { __ } = wp.i18n;
 const { Component } = wp.element;
 const {
   InspectorControls,
+  ColorPalette,
 } = wp.blockEditor;
 
 const {
@@ -12,6 +13,7 @@ const {
 } = wp.components;
 
 import HeadingToolbar from './HeadingToolbar';
+import FontSizePicker from '../TextControls/FontSizePicker';
 
 /**
  * Create an Inspector Controls wrapper Component
@@ -25,6 +27,7 @@ export default class Inspector extends Component {
     const {
       attributes: {
         level,
+        textColor,
       },
       setAttributes
     } = this.props;
@@ -35,8 +38,18 @@ export default class Inspector extends Component {
           title={__("Title Settings", "paper-blocks")}
           initialOpen={false}
         >
-            <p>{ __( 'Level' ) }</p>
-            <HeadingToolbar isCollapsed={ false } minLevel={ 1 } maxLevel={ 7 } selectedLevel={ level } onChange={ ( newLevel ) => setAttributes( { level: newLevel } ) } />
+
+                <p>{ __( 'Level' ) }</p>
+                <HeadingToolbar isCollapsed={ false } minLevel={ 1 } maxLevel={ 7 } selectedLevel={ level } onChange={ ( newLevel ) => setAttributes( { level: newLevel } ) } />
+
+                <p>{ __( 'Color' ) }</p>
+                <ColorPalette
+                    value={textColor}
+                    onChange={textColor => {
+                        setAttributes({ textColor });
+                    }}
+                />
+                <FontSizePicker {...{ setAttributes, ...this.props }} />
         </PanelBody>
       </InspectorControls>
     );
