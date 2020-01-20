84a8b4e83a826995b219460d1976aa5fcb03c07b
freddiemixell <fmixell@gmail.com>
2020-01-20 04:54:40 -0500

    using function component

--- blocks/title/Inspector.js
+++ blocks/title/Inspector.js
@@ -5,46 +5,34 @@ import TextStyle from '../TextControls/TextStyle';
 import TextColor from '../TextControls/TextColor';
 
 const { __ } = wp.i18n;
-const { Component } = wp.element;
 const { InspectorControls } = wp.blockEditor;
 const {  PanelBody } = wp.components;
 
-export default class Inspector extends Component {
-  constructor() {
-    super(...arguments);
-  }
-
-  render() {
-    const {
-      attributes: {
-      level,
-      textColor,
-    },
-      setAttributes
-    } = this.props;
-
-    return (
-      <InspectorControls>
-        <PanelBody
-          title={__("Title Settings", "paper-blocks")}
-          initialOpen={false}
-        >
-          <HeadingToolbar
-            isCollapsed={ false }
-            minLevel={ 1 }
-            maxLevel={ 7 }
-            selectedLevel={ level }
-            onChange={ ( newLevel ) => setAttributes( { level: newLevel } ) }
-          />
-          <TextColor
-            setAttributes={ setAttributes }
-            textColor={ textColor }
-          />
-          <FontSizePicker {...{ setAttributes, ...this.props }} />
-          <TextTransform {...{ setAttributes, ...this.props }} />
-          <TextStyle {...{ setAttributes, ...this.props }} />
-        </PanelBody>
-      </InspectorControls>
-    );
-  }
+const Inspector = ( props ) => {
+  const { attributes: { level, textColor }, setAttributes } = props;
+  return (
+    <InspectorControls>
+      <PanelBody
+        title={ __( 'Title Settings', 'paper-blocks' ) }
+        initialOpen={false}
+      >
+        <HeadingToolbar
+          isCollapsed={ false }
+          minLevel={ 1 }
+          maxLevel={ 7 }
+          selectedLevel={ level }
+          onChange={ ( newLevel ) => setAttributes( { level: newLevel } ) }
+        />
+        <TextColor
+          setAttributes={ setAttributes }
+          textColor={ textColor }
+        />
+        <FontSizePicker {...{ setAttributes, ...props }} />
+        <TextTransform {...{ setAttributes, ...props }} />
+        <TextStyle {...{ setAttributes, ...props }} />
+      </PanelBody>
+    </InspectorControls>
+  );
 }
+
+export default Inspector;
