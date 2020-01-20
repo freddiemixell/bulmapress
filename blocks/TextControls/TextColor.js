c8b1d333be4327f6bc20488a7c44cff95f666b7f
freddiemixell <fmixell@gmail.com>
2020-01-20 04:49:59 -0500

    using this to wrap text color

--- blocks/TextControls/TextColor.js
+++ blocks/TextControls/TextColor.js
@@ -1,64 +1,19 @@
-const { createElement } = wp.element
-const { registerFormatType, applyFormat, removeFormat, getActiveFormat } = wp.richText
-const { InspectorControls, PanelColorSettings } = wp.blockEditor;
+import PanelSection from '../Controls/PanelSection';
+
+const { ColorPalette } = wp.blockEditor;
 const { __ } = wp.i18n;
 
-[
-  {
-    name: 'color',
-    title: 'Inline Text Color'
-  },
-  {
-    name: 'background-color',
-    title: 'Inline Background Color'
-  }
-].forEach(({ name, title }) => {
-  const type = `paperblocks/${name}`
+const TextColor = ( { textColor, setAttributes } ) => {
+  return (
+    <PanelSection label={ __( 'Color', 'paper-blocks' ) }>
+      <ColorPalette
+        value={textColor}
+        onChange={textColor => {
+          setAttributes({ textColor });
+        }}
+      />
+    </PanelSection>
+  );
+}
 
-  registerFormatType(type, {
-    title,
-    tagName: 'span',
-    className: name,
-    attributes: {
-      style: 'style'
-    },
-    edit ({ isActive, value, onChange }) {
-      let activeColor
-
-      if (isActive) {
-        const activeFormat = getActiveFormat(value, type)
-        const style = activeFormat.attributes.style
-
-        activeColor = style.replace(new RegExp(`^${name}:\\s*`), '')
-      }
-
-      return (
-        <InspectorControls>
-          <PanelColorSettings
-            title={title}
-            initialOpen={false}
-            colorSettings={[
-              {
-                value: activeColor,
-                onChange: (color) => {
-                  if (color) {
-                    onChange(applyFormat(value, {
-                      type,
-                      attributes: {
-                        style: `${name}:${color}`
-                      }
-                    }))
-                    return
-                  }
-
-                  onChange(removeFormat(value, type))
-                },
-                label: 'Apply color to the selected text.'
-              }
-            ]}
-          />
-        </InspectorControls>
-      )
-    }
-  })
-})
+export default TextColor;
