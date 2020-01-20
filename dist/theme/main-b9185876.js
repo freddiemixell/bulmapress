fb1dbb0c2b051ee8c6de1c77f275401438b7b86a
freddiemixell <fmixell@gmail.com>
2020-01-20 06:38:15 -0500

    built after adding subtitle

---
+++ dist/theme/main-b9185876.js
@@ -0,0 +1,19 @@
+/*!
+ * 
+ * paperPress
+ * 
+ * @author 
+ * @version 0.2.0
+ * @link UNLICENSED
+ * @license UNLICENSED
+ * 
+ * Copyright (c) 2020 
+ * 
+ * This software is released under the UNLICENSED License
+ * https://opensource.org/licenses/UNLICENSED
+ * 
+ * Compiled with the help of https://wpack.io
+ * A zero setup Webpack Bundler Script for WordPress
+ */
+(window.wpackiopaperPressthemeJsonp=window.wpackiopaperPressthemeJsonp||[]).push([[2],{10:function(e,p,s){"use strict";var n="paperPressdist".replace(/[^a-zA-Z0-9_-]/g,"");s.p=window["__wpackIo".concat(n)]},18:function(e,p,s){s(10),e.exports=s(19)},19:function(e,p,s){"use strict";s.r(p);s(20)},20:function(e,p,s){}},[[18,0]]]);
+//# sourceMappingURL=main-b9185876.js.map
