35033d69cdb2393adeca72b37cd236fde94c3f38
freddiemixell <fmixell@gmail.com>
2020-01-20 12:22:24 -0500

    button color

--- blocks/library/elements/button/edit.js
+++ blocks/library/elements/button/edit.js
@@ -5,19 +5,27 @@ import Controls from './controls';
 const { Fragment } = wp.element;
 const { RichText } = wp.blockEditor;
 
-const allowedFormats = [ 'core/italic', 'core/link', 'paperblocks/color', 'paperblocks/background-color' ];
+const allowedFormats = [ 'paperblocks/color', 'paperblocks/background-color' ];
 
 const edit = props => {
-    const { attributes: { textContent }, setAttributes } = props;
+    const { attributes: { textContent, buttonColor }, setAttributes } = props;
     const onChangeText = textContent => { setAttributes( { textContent } ) };
     const classNames = getClassNames( props );
     const style = inlineStyles( props );
+    const buttonStyle = {}
+
+    if ( typeof buttonColor !== 'undefined' ) {
+        buttonStyle.backgroundColor = buttonColor;
+    }
 
     return (
         <Fragment>
             <Controls {...{ setAttributes, ...props }} />
             <Inspector {...{ setAttributes, ...props }} />
-            <button className={ `paperpress-button paperpress-is-primary ${ classNames }` }>
+            <button
+                className={ `paperpress-button paperpress-is-primary ${ classNames }` }
+                style={ buttonStyle }
+            >
                 <RichText
                     tagName={ `span` }
                     placeholder="Button"
