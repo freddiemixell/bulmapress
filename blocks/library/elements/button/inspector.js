765857d31810f81f69b701fe9da2e640d5d08ecb
freddiemixell <fmixell@gmail.com>
2020-01-20 16:08:01 -0500

    add button color control

--- blocks/library/elements/button/inspector.js
+++ blocks/library/elements/button/inspector.js
@@ -1,13 +1,14 @@
 import FontSizePicker from '../../../editor/textcontrols/textsize';
 import TextTransform from '../../../editor/textcontrols/texttransform';
 import TextColor from '../../../editor/textcontrols/textcolor';
+import ButtonColor from '../../../editor/buttoncontrols/buttoncolor';
 
 const { __ } = wp.i18n;
 const { InspectorControls } = wp.blockEditor;
 const {  PanelBody } = wp.components;
 
 const Inspector = ( props ) => {
-  const { attributes: { textColor }, setAttributes } = props;
+  const { attributes: { textColor, buttonColor }, setAttributes } = props;
   return (
     <InspectorControls>
       <PanelBody
@@ -17,6 +18,10 @@ const Inspector = ( props ) => {
         <TextColor
           setAttributes={ setAttributes }
           textColor={ textColor }
+        />
+        <ButtonColor
+          setAttributes={ setAttributes }
+          buttonColor={ buttonColor }
         />
         <FontSizePicker {...{ setAttributes, ...props }} />
         <TextTransform {...{ setAttributes, ...props }} />
