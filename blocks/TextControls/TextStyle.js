8115ec3fe3b18ce793cdb06762818e0b256911d4
freddiemixell <fmixell@gmail.com>
2020-01-20 03:05:02 -0500

    adding more text styles

--- blocks/TextControls/TextStyle.js
+++ blocks/TextControls/TextStyle.js
@@ -4,7 +4,9 @@ const { __ } = wp.i18n;
 const TextStyle = ( {
     setAttributes,
     attributes: {
-        italic
+        italic,
+        weight,
+        alignment,
     },
 } ) => {
     return (
@@ -18,6 +20,42 @@ const TextStyle = ( {
                     isPressed={italic === 'paperpress-is-italic'}
                     onClick={() => {
                         setAttributes({italic: italic !== 'paperpress-is-italic' ? 'paperpress-is-italic' : ''})
+                    }}
+                />
+                <Button
+                    icon='editor-bold'
+                    label='bold'
+                    className="components-toolbar__control"
+                    isPressed={weight === 'paperpress-has-text-weight-bold'}
+                    onClick={() => {
+                        setAttributes({weight: weight !== 'paperpress-has-text-weight-bold' ? 'paperpress-has-text-weight-bold' : ''})
+                    }}
+                />
+                <Button
+                    icon='editor-alignleft'
+                    label='left align'
+                    className="components-toolbar__control"
+                    isPressed={alignment === 'paperpress-has-text-left'}
+                    onClick={() => {
+                        setAttributes({alignment: alignment !== 'paperpress-has-text-left' ? 'paperpress-has-text-left' : ''})
+                    }}
+                />
+                <Button
+                    icon='editor-aligncenter'
+                    label='center align'
+                    className="components-toolbar__control"
+                    isPressed={alignment === 'paperpress-has-text-centered'}
+                    onClick={() => {
+                        setAttributes({alignment: alignment !== 'paperpress-has-text-centered' ? 'paperpress-has-text-centered' : ''})
+                    }}
+                />
+                <Button
+                    icon='editor-alignright'
+                    label='right align'
+                    className="components-toolbar__control"
+                    isPressed={alignment === 'paperpress-has-text-right'}
+                    onClick={() => {
+                        setAttributes({alignment: alignment !== 'paperpress-has-text-right' ? 'paperpress-has-text-right' : ''})
                     }}
                 />
             </div>
