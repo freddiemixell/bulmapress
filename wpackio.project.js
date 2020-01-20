54fa0f7ddfab3d04f16fd4c92d900e52bfa89ebd
freddiemixell <fmixell@gmail.com>
2020-01-20 12:48:32 -0500

    moving sass out of inc

--- wpackio.project.js
+++ wpackio.project.js
@@ -29,7 +29,7 @@ module.exports = {
 		{
 			name: 'theme',
 			entry: {
-				main: ['./src/theme/index.js'],
+				main: ['./js/theme/index.js'],
 				paperBlocks: [ './blocks/library/index.js' ],
 			},
 			webpackConfig: (config, merge, appDir, isDev) => {
