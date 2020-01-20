e5ac7c7ba2f01ae44a088b027510afb7d5e84286
freddiemixell <fmixell@gmail.com>
2020-01-20 00:18:43 -0500

    text transform control

---
+++ blocks/TextControls/TextTransform.js
@@ -0,0 +1,36 @@
+const { SelectControl, Button } = wp.components;
+const { Fragment } = wp.element;
+const { __ } = wp.i18n;
+
+const options = [
+    { label: "Capitalized", value: "paperpress-is-capitalized" },
+    { label: "Lowercase", value: "paperpress-is-lowercase" },
+    { label: "Uppercase", value: "paperpress-is-uppercase" },
+    { label: "Italic", value: "paperpress-is-italic" },
+];
+
+export default function TextTransform( props ) {
+    const {
+        attributes: {
+            transform
+        },
+        setAttributes,
+    } = props;
+    return (
+        <Fragment>
+            <SelectControl
+                label={ __( 'Transform' ) }
+                options={ options }
+                value={ transform }
+                onChange={ transform => setAttributes( { transform } ) }
+            />
+            <Button
+                isSmall
+                isSecondary
+                onClick={ () => setAttributes( { transform: undefined } ) }
+            >
+                Reset
+            </Button>
+        </Fragment>
+    );
+}
