2dec00e68a4b98a0ec040e39a386d9d72dc7571f
freddiemixell <fmixell@gmail.com>
2020-01-20 16:08:19 -0500

    using link settings in button save

--- blocks/library/elements/button/save.js
+++ blocks/library/elements/button/save.js
@@ -1,7 +1,7 @@
 import { getClassNames, inlineStyles } from '../../../editor/textcontrols/helpers';
 
 const save = props => {
-    const { textContent, buttonColor } = props.attributes;
+    const { textContent, buttonColor, url, target } = props.attributes;
     const classNames = getClassNames( props );
     const style = inlineStyles( props );
     const buttonStyle = {}
@@ -10,16 +10,24 @@ const save = props => {
         buttonStyle.backgroundColor = buttonColor;
     }
 
+    let linkProps = {}
+    if ( target ) {
+        linkProps.target = '_blank'
+        linkProps.rel = 'noopener noreferrer'
+    }
+
     return (
-        <button
+        <a
+            href={ url }
             className={ `paperpress-button paperpress-is-primary ${ classNames }` }
             style={ buttonStyle }
+            { ...linkProps }
         >
             <span style={ style }>{ textContent }</span>
             <span className="paperpress-icon">
                 <i className="fas fa-arrow-right"></i>
             </span>
-        </button>
+        </a>
     );
 }
 
