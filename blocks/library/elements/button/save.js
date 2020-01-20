3496e071c20df9e441874ea6d199c61f327be89a
freddiemixell <fmixell@gmail.com>
2020-01-20 12:22:35 -0500

    button color frontend

--- blocks/library/elements/button/save.js
+++ blocks/library/elements/button/save.js
@@ -1,12 +1,20 @@
 import { getClassNames, inlineStyles } from '../../../editor/textcontrols/helpers';
 
 const save = props => {
-    const { textContent } = props.attributes;
+    const { textContent, buttonColor } = props.attributes;
     const classNames = getClassNames( props );
     const style = inlineStyles( props );
+    const buttonStyle = {}
+
+    if ( typeof buttonColor !== 'undefined' ) {
+        buttonStyle.backgroundColor = buttonColor;
+    }
 
     return (
-        <button className={ `paperpress-button paperpress-is-primary ${ classNames }` }>
+        <button
+            className={ `paperpress-button paperpress-is-primary ${ classNames }` }
+            style={ buttonStyle }
+        >
             <span style={ style }>{ textContent }</span>
             <span className="paperpress-icon">
                 <i className="fas fa-arrow-right"></i>
