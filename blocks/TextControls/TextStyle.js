c8966331d57b6214e402e6f402eb76449de243be
freddiemixell <fmixell@gmail.com>
2020-01-20 02:48:24 -0500

    styling text style area

--- blocks/TextControls/TextStyle.js
+++ blocks/TextControls/TextStyle.js
@@ -1,13 +1,26 @@
-const { PanelRow, IconButton } = wp.components;
+const { Button } = wp.components;
 const { __ } = wp.i18n;
 
-const TextStyle = () => {
+const TextStyle = ( {
+    setAttributes,
+    attributes: {
+        italic
+    },
+} ) => {
     return (
-        <div className="components-toolbar">
-                <IconButton
+        <div style={{marginBottom: '19.5px'}}>
+            <p style={{paddingBottom: '13px'}}>{ __( 'Text Style' ) }</p>
+            <div className="components-toolbar">
+                <Button
                     icon='editor-italic'
                     label='italic'
+                    className="components-toolbar__control"
+                    isPressed={italic === 'paperpress-is-italic'}
+                    onClick={() => {
+                        setAttributes({italic: italic !== 'paperpress-is-italic' ? 'paperpress-is-italic' : ''})
+                    }}
                 />
+            </div>
         </div>
     )
 }
