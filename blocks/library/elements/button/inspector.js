2280bc1ddc81ce157fe469a1c5cafdf83cb9c21c
freddiemixell <fmixell@gmail.com>
2020-01-20 19:36:58 -0500

    button style in the inspector

--- blocks/library/elements/button/inspector.js
+++ blocks/library/elements/button/inspector.js
@@ -3,19 +3,25 @@ import TextTransform from '../../../editor/textcontrols/texttransform';
 import TextColor from '../../../editor/textcontrols/textcolor';
 import ButtonColor from '../../../editor/buttoncontrols/buttoncolor';
 import ButtonSubset from '../../../editor/buttoncontrols/buttonsubset';
+import ButtonStyle from '../../../editor/buttoncontrols/buttonstyle';
 
 const { __ } = wp.i18n;
 const { InspectorControls } = wp.blockEditor;
 const {  PanelBody } = wp.components;
 
 const Inspector = ( props ) => {
-  const { attributes: { textColor, buttonColor, buttonSubset }, setAttributes } = props;
+  const { attributes: { textColor, buttonColor, buttonSubset, isPrimary }, setAttributes } = props;
   return (
     <InspectorControls>
       <PanelBody
         title={ __( 'Button Settings', 'paper-blocks' ) }
-        initialOpen={false}
+        initialOpen={true}
       >
+        <ButtonSubset
+          setAttributes={ setAttributes }
+          buttonSubset={ buttonSubset }
+        />
+        <ButtonStyle {...{ setAttributes, ...props }} />
         <TextColor
           setAttributes={ setAttributes }
           textColor={ textColor }
@@ -26,10 +32,6 @@ const Inspector = ( props ) => {
         />
         <FontSizePicker {...{ setAttributes, ...props }} />
         <TextTransform {...{ setAttributes, ...props }} />
-        <ButtonSubset
-          setAttributes={ setAttributes }
-          buttonSubset={ buttonSubset }
-        />
       </PanelBody>
     </InspectorControls>
   );
